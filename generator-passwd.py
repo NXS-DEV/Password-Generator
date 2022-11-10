9ab34a1855a64345bd1d2df158e8bcd25965a474
NXS-DEV <105024951+NXS-DEV@users.noreply.github.com>
2022-11-10 14:47:00 -0500

    Update generator-passwd.py
    Version = "0.0.0.1.3"
    Module tkinter ajoutée.
    GUI ajoutée.
    Le mode console désactivé.

--- generator-passwd.py
+++ generator-passwd.py
@@ -1,19 +1,35 @@
-
 import string
 import random
+import tkinter as tk
+from tkinter import ttk
 
 letters = string.ascii_letters
 numbers = string.digits
 symbols = string.punctuation
-
 generate = letters + numbers + symbols
 
 length = 20
 
 password = "".join(random.sample(generate, length))
 
-print(password)
+# print(password) Console mode deactivated
+version = "0.0.0.1.3"
+# create a window
+root = tk.Tk()
+print("Welcome on Password Generator")
+print("Developped by : Noxious")
+print("Gen-Version: " + version)
+name = input("Enter your name : ")
+# size of the window
+root.geometry("450x250")
 
+# window title
+root.title("Password Generator | Developed by : Noxious")
 
+# main text
+tk.Label(text="Welcome " + name + ", This is your password:", foreground="red", background="grey", width=100, font="Arial").pack()
+tk.Label(text=f"{password}", background="grey", width="100", height="50", foreground="red", font="Arial").pack()
 
-# Next update : Add GUI(Graphic User Interface)
+# Main loop
+root.mainloop()
+# Next Update : GUI Update : Better design
